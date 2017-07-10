826aef7f7a19bde4ddb6c83c40893c169d5608be
Loren Sheets <lorenmsheets@gmail.com>
2017-07-09 21:09:54 -0700

    changes

--- script2.py
+++ script2.py
@@ -4,4 +4,5 @@ context = ssl._create_unverified_context()
 
 with urllib.request.urlopen("https://spaceinfo.herokuapp.com/spacexjobs",context=context) as url:
     data = json.loads(url.read().decode())
-    print(data)
+    for item in data:
+        print(data[item])
