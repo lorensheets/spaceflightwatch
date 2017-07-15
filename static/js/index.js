69ec1f0b06c6a19732f7ef34892f42b5933aa477
Loren Sheets <lorenmsheets@gmail.com>
2017-07-15 11:44:39 -0700

    layout change

--- static/js/index.js
+++ static/js/index.js
@@ -129,7 +129,7 @@ $(document).ready(function() {
 
     var thisTxt = $(this).text() == 'View More' ? 'View Less' : 'View More';
     var launchContainerHeight = $('.launch-table').css('height') == '200px' ? window.innerHeight * 0.7 + 'px' : '200px';
-    var launchTableHeight = $('.launch-table-container').css('height') == '190px' ? ( window.innerHeight * 0.7 ) - 10 + 'px' : '190px';
+    var launchTableHeight = $('.launch-table-container').css('height') == '190px' ? ( window.innerHeight * 0.7 ) - 12 + 'px' : '190px';
     var scroll = $('#launchTable').offset().top - 140;
     var overflow = $('.launch-table-container').css('overflow') == 'scroll' ? 'hidden' : 'scroll';
 
