39ecaad469eff22c4a5405d33ef9c2f8bca151aa
Loren Sheets <lorenmsheets@gmail.com>
2017-07-24 01:06:33 -0700

    changes

--- static/js/resize.js
+++ static/js/resize.js
@@ -1,39 +1,29 @@
 $(document).ready(function() {
 
-  $(window).on("orientationchange", function() {
-    // portrait mode
-    if(window.innerHeight > window.innerWidth){
+  livestream_size = function() {
+    let w = window.innerWidth * 0.95;
+    let h = w * 0.56;
+    let m = "5px auto " + w * 0.05 + "px auto";
+    let m2 = w * 0.02 + "px auto 0 auto";
+    $('#livestream').css({
+      'display': 'block',
+      'width': w,
+      'height': h,
+      'margin': m
+    });
+    $('.watch-live').css({
+      'display': 'block',
+      'width': w,
+      'margin': m2
+    });
+  }
 
-    } else {  // landscape mode
+  if (live_stream == true) {
 
-    }
-  });
+    $(window).on("orientationchange", livestream_size);
 
+    $(window).on("resize", livestream_size);
 
-  $(window).on("resize", function() {
-
-    console.log(live_stream);
-    
-    if (live_stream == true) {
-
-      var w = window.innerWidth * 0.95;
-      var h = w * 0.56;
-      var m = "5px auto " + w * 0.05 + "px auto";
-      var m2 = w * 0.02 + "px auto 0 auto";
-
-      $('#livestream').css({
-        'display': 'block',
-        'width': w,
-        'height': h,
-        'margin': m
-      });
-      $('.watch-live').css({
-        'display': 'block',
-        'width': w,
-        'margin': m2
-      });
-
-    }
-  });
+  }
 
 });
