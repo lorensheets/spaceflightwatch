762045ed0c852f3fcff2e3ddb2221c5fdc0424f2
Loren Sheets <lorenmsheets@gmail.com>
2017-07-24 00:23:46 -0700

    changes

--- static/js/resize.js
+++ static/js/resize.js
@@ -1,13 +1,13 @@
 $(window).on("orientationchange", function() {
   // portrait mode
   if(window.innerHeight > window.innerWidth){
-    alert("portrait");
+
   } else {  // landscape mode
-    alert("landscape");
+
   }
 });
 
 
 $(window).on("resize", function() {
-  alert("resize");
+
 });
