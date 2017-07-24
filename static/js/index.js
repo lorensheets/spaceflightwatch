1bc2df1feccd986790c36abf1c35bd2942a2e0be
Loren Sheets <lorenmsheets@gmail.com>
2017-07-24 00:29:27 -0700

    changes

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+  var livestream;
+
   var y = new Date().getFullYear();
   date = date + " " + y;
   function formatDate(date) {
@@ -104,6 +106,7 @@ $(document).ready(function() {
   // Watch Live script
 
   if ( (time_diff/1000 < 777600) && (time_diff/1000 > -3600)) {
+    livestream = true;
     var w = window.innerWidth * 0.95;
     var h = w * 0.56;
     var m = "5px auto " + w * 0.05 + "px auto";
@@ -120,6 +123,8 @@ $(document).ready(function() {
       'width': w,
       'margin': m2
     });
+  } else {
+    livestream = false;
   }
 
 
--- static/js/resize.js
+++ static/js/resize.js
@@ -10,4 +10,24 @@ $(window).on("orientationchange", function() {
 
 $(window).on("resize", function() {
 
+  if (livestream == true) {
+
+    var w = window.innerWidth * 0.95;
+    var h = w * 0.56;
+    var m = "5px auto " + w * 0.05 + "px auto";
+    var m2 = w * 0.02 + "px auto 0 auto";
+
+    $('#livestream').css({
+      'display': 'block',
+      'width': w,
+      'height': h,
+      'margin': m
+    });
+    $('.watch-live').css({
+      'display': 'block',
+      'width': w,
+      'margin': m2
+    });
+    
+  }
 });
