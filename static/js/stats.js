dd677e2c08b205bb0811c5535518be3d2b54a20b
Loren Sheets <lorenmsheets@gmail.com>
2017-07-24 01:31:05 -0700

    changes

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $.getJSON("http://api.open-notify.org/astros.json", function(data) {
     let numPeople = data.number;
-    $('#number_of_people').html(numPeople);
+    $('#people_in_space').html(numPeople);
   });
 });
