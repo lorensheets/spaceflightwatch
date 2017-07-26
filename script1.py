a8a91bca2da06302f3cb093301d470524c029538
Loren Sheets <lorenmsheets@gmail.com>
2017-07-25 23:44:21 -0700

    changes

--- script1.py
+++ script1.py
@@ -144,5 +144,4 @@ def test():
     #next launch
     with urllib.request.urlopen("http://spaceflight.watch/nextlaunch") as url:
         nextlaunch = json.loads(url.read().decode())
-    return render_template("test.html",date=nextlaunch[3],time=nextlaunch[0],
-        link=nextlaunch[2],next=nextlaunch[1])
+    return render_template("test.html",nextlaunch=nextlaunch)
