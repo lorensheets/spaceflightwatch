79aeca2ac1d0ee5c27148b3485f24023f6bd4647
Loren Sheets <lorenmsheets@gmail.com>
2017-07-28 14:00:24 -0700

    changes

--- static/js/index.js
+++ static/js/index.js
@@ -137,7 +137,7 @@ $(document).ready(function() {
     var thisTxt = $(this).text() == 'View More' ? 'View Less' : 'View More';
     var launchTableHeight = $('.launch-table').css('height'); console.log(launchTableHeight);
     if (launchTableHeight == '200px') {
-      var newlaunchTableHeight = $('.launch-table').css('height', 'auto').height(); console.log(newlaunchTableHeight);
+      var newlaunchTableHeight = $('.launch-table').css('height', '100%').height(); console.log(newlaunchTableHeight);
       $('.launch-table').animate({
         'height': newlaunchTableHeight
       }, 500, 'easeInOutQuad');
