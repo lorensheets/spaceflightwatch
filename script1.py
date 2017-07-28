62686673abc5464b494f6c936327be96d54fdb9b
Loren Sheets <lorenmsheets@gmail.com>
2017-07-27 23:22:38 -0700

    changes

--- script1.py
+++ script1.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, Response
 import nextlaunch, urllib.request, json, ssl
 
-from redis import Redis
-redis = Redis()
-
-import time
-from datetime import datetime
-
 
 
 app=Flask(__name__)
@@ -150,44 +144,3 @@ def test():
     with urllib.request.urlopen("http://spaceflight.watch/nextlaunch") as url:
         nextlaunch = json.loads(url.read().decode())
     return render_template("test.html",nextlaunch=nextlaunch)
-
-
-
-#
-#   redis
-#
-
-ONLINE_LAST_MINUTES = 5
-
-def mark_online(user_id):
-    now = int(time.time())
-    expires = now + (app.config['ONLINE_LAST_MINUTES'] * 60) + 10
-    all_users_key = 'online-users/%d' % (now // 60)
-    user_key = 'user-activity/%s' % user_id
-    p = redis.pipeline()
-    p.sadd(all_users_key, user_id)
-    p.set(user_key, now)
-    p.expireat(all_users_key, expires)
-    p.expireat(user_key, expires)
-    p.execute()
-
-def get_user_last_activity(user_id):
-    last_active = redis.get('user-activity/%s' % user_id)
-    if last_active is None:
-        return None
-    return datetime.utcfromtimestamp(int(last_active))
-
-def get_online_users():
-    current = int(time.time()) // 60
-    minutes = xrange(app.config['ONLINE_LAST_MINUTES'])
-    return redis.sunion(['online-users/%d' % (current - x)
-                         for x in minutes])
-
-@app.before_request
-def mark_current_user_online():
-    mark_online(request.remote_addr)
-
-@app.route('/online')
-def index():
-    return Response('Online: %s' % ', '.join(get_online_users()),
-                    mimetype='text/plain')
